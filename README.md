58e4aba664ceda9131a5b8eba0a251da8658a40a
Claudio Bandera <claudio.bandera@kit.edu>
2016-06-18 15:34:57 +0200

    Added example file and filled README

--- README.md
+++ README.md
@@ -1,10 +1,40 @@
 # stargazer
 A landmark-based visual indoor localization system for mobile robots
-
+![Detected Landmarks](res/detected_landmarks.png)
 
 ## Installation
-You need the following dependencies to build this project: 
+### Dependencies
+You need the following dependencies to build this project:
 
 * [yaml-cpp](https://github.com/jbeder/yaml-cpp/)
 * [libceres-dev](https://ceres-solver.org/)
 * [opencv](https://opencv.org/)
+
+If you are using [rosdep](http://docs.ros.org/independent/api/rosdep/html/overview.html), which now is a stand-alone tool independent of ROS, you can simply run the following command to install these dependencies:
+
+    rosdep install stargazer
+
+## Build
+[catkin-tools](https://catkin-tools.readthedocs.io/en/latest/) are recommended as build tools, but a classical build is possible as well:
+```
+mkdir build
+cd build
+cmake ..
+make
+```
+
+## Install
+Install via
+
+    make install
+
+## Example
+A simple usage example is included and will be build with the library. It resides in `devel/lib/stargazer/` run it by handing it the path to the example file and config:
+
+    ./devel/lib/stargazer/stargazer_demo res/frame0135.jpg res/stargazer.yaml-cpp
+
+
+# Documentation
+The library is fully documented with Doxygen comments. Build the documentation by running
+
+    doxygen Doxyfile
---
+++ example/demo.cpp
@@ -0,0 +1,54 @@
+#include <CeresLocalizer.h>
+#include <DebugVisualizer.h>
+#include "LandmarkFinder.h"
+
+using namespace stargazer;
+using namespace std;
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        cout << " Usage: " << argv[0] << " <image_file> <config_file>" << endl;
+        return -1;
+    }
+    cv::Mat input_image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR); // Read the file
+
+    DebugVisualizer debugVisualizer;
+    debugVisualizer.SetWaitTime(-1); // Wait until user has pressed key
+    debugVisualizer.SetWindowMode(CV_WINDOW_NORMAL);
+
+    LandmarkFinder landmarkFinder(argv[2]);
+    std::vector<ImgLandmark> detected_landmarks;
+    landmarkFinder.DetectLandmarks(input_image, detected_landmarks);
+
+    cout << "Displaying images, press any key to continue.... " << endl;
+
+    // Invert images for better visibilty
+    cv::bitwise_not(landmarkFinder.rawImage_, landmarkFinder.rawImage_);
+    cv::bitwise_not(landmarkFinder.grayImage_, landmarkFinder.grayImage_);
+    cv::bitwise_not(landmarkFinder.filteredImage_, landmarkFinder.filteredImage_);
+
+    // Show images
+    debugVisualizer.ShowImage(landmarkFinder.rawImage_, "Raw Image");
+    debugVisualizer.ShowImage(landmarkFinder.grayImage_, "Gray Image");
+    debugVisualizer.ShowImage(landmarkFinder.filteredImage_, "Filtered Image");
+
+    // Show detections
+    auto point_img = debugVisualizer.ShowPoints(landmarkFinder.filteredImage_, landmarkFinder.clusteredPixels_);
+    auto cluster_img = debugVisualizer.ShowClusters(landmarkFinder.filteredImage_, landmarkFinder.clusteredPoints_);
+
+    // Show landmarks
+    cv::Mat temp = landmarkFinder.rawImage_.clone();
+    debugVisualizer.DrawLandmarks(temp, detected_landmarks);
+    debugVisualizer.ShowImage(temp, "Detected Landmarks");
+
+    // Localize
+    CeresLocalizer localizer(argv[2]);
+    localizer.UpdatePose(detected_landmarks, 0.0);
+    cout << localizer.getSummary().FullReport() << endl << endl;
+
+    pose_t pose = localizer.getPose();
+    cout << "Pose is x=" << pose[(int)POSE::X] << " y=" << pose[(int)POSE::Y] << " z=" << pose[(int)POSE::Z]
+         << " rx=" << pose[(int)POSE::Rx] << " ry=" << pose[(int)POSE::Ry] << " rz=" << pose[(int)POSE::Rz] << endl;
+
+    return EXIT_SUCCESS;
+}
